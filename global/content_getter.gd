917166754ff4f95d52458a6d22a600408b572322
VlC <victorstancioiu@gmail.com>
2023-05-09 23:52:57 +0200

    more rewriting contentgetter

--- global/content_getter.gd
+++ global/content_getter.gd
@@ -1,7 +1,10 @@
 extends Node
 
+const URL_DB: String = "https://github.com/vlcoo/MvLO-ModLoader/raw/main/DB.zip"
+const URL_GAMEFILES: String = "http://mvloml.vlcoo.net/DB.gamefiles.json"
+
 @onready var requester_db: HTTPRequest = $HTTPRequestDB
-@onready var requester_timestamp: HTTPRequest = $HTTPRequestTimestamp
+@onready var requester_gamefiles: HTTPRequest = $HTTPRequestGamefiles
 
 var db_request_complete = false
 var gamefiles_request_complete = false
@@ -14,30 +17,51 @@ signal cache_updated(succeeded: bool)
 func _on_ready() -> void:
 	if Configurator.cache_is_old or not _check_dbs_integrity():
 		$AnimationPlayer.play("in")
-		# request dbs
+		var error = requester_db.request(URL_DB) + requester_gamefiles.request(URL_GAMEFILES)
+		if error != OK: err(str(error))
 	else:
-		pass
+		_populate_moddata_array()
 
 
 func _on_requester_db_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
-	# parse first
-	_unpack_db()
+	var reader: ZIPReader = ZIPReader.new()
+	var error: Error = reader.open(requester_db.download_file)
+	if error != OK:
+		err("Unpacking failed. " + str(error))
+		return
 
-func _unpack_db() -> void:
-	# extract db from zip and save
+	for filename in reader.get_files():
+		if filename.ends_with("/"):
+			DirAccess.make_dir_absolute("user://" + filename)
+		else:
+			var file = FileAccess.open("user://" + filename, FileAccess.READ_WRITE if FileAccess.file_exists("user://" + filename) else FileAccess.WRITE)
+			file.store_buffer(reader.read_file(filename))
+
+	reader.close()
 
 	db_request_complete = true
 	if gamefiles_request_complete: _populate_moddata_array()
 
 
 func _on_requester_gamefiles_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
-	# parse first
 	gamefiles_request_complete = true
 	if db_request_complete: _populate_moddata_array()
 
 
 func _populate_moddata_array() -> void:
-	# read from db folder
+	if not _check_dbs_integrity():
+		err("DB not present locally.")
+		return
+
+	var dir = DirAccess.open("user://DB/mod_datas")
+	for filename in dir.get_files():
+		var mod_id: String = filename.replace(".tres", "")
+		var data: ModData = load(dir.get_current_dir() + "/" + filename)
+		data.cover_image = load("user://DB/" + mod_id + "C.png")
+		if FileAccess.file_exists("user://DB/" + mod_id + "I.png"):
+			data.icon = load("user://DB/" + mod_id + "I.png")
+		moddatas.append(data)
+
 	emit_signal("cache_updated", true)
 	$AnimationPlayer.play("out")
 	pass
@@ -45,3 +69,10 @@ func _populate_moddata_array() -> void:
 
 func _check_dbs_integrity() -> bool:
 	return FileAccess.file_exists("user://DB.gamefiles.json") and DirAccess.dir_exists_absolute("user://DB")
+
+
+func err(text: String):
+	$AcceptDialog.dialog_text = "Database could not be downloaded! Some info might be out of date.\n" + "Cause: " + text
+	$AcceptDialog.popup_centered()
+	emit_signal("cache_updated", false)
+	$AnimationPlayer.play("out")
